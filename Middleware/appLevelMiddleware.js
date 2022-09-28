eacc2d05470f91bd425f374d1487777cfaf8f29b
Shubham Sareliya <sareliashubham9999@gmail.com>
2022-09-28 13:02:32 +0530

    Middleware Module Updated

---
+++ Middleware/appLevelMiddleware.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.listen(5000, () => { console.log("Server running on port 5000"); });
+
+const middlewareFunction = function (req, res, next) {
+    console.log("Hello from middleware function");
+    next();
+}
+
+app.use(middlewareFunction);
+
+app.get('/', (req, res) => {
+    res.send("From home page");
+})
+
+app.get('/about', (req, res) => {
+    res.send("From about");
+})
