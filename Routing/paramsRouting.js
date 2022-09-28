6f696844aec607255b296816b168d38f8265198c
Meet Bharodiya <97339113+MeetBharodiya@users.noreply.github.com>
2022-09-28 18:50:49 +0530

    Updated file

--- Routing/paramsRouting.js
+++ Routing/paramsRouting.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Request from get");
+app.listen(5000,()=>{console.log("Running........");});
+
+app.get('/',(req,res)=>{
+    res.send("You are in Home page of Nike.com");
 })
 
-app.get('/about', (req, res) => {
-  let temp = req.query.temp;
-  let page = req.query.page;
-  res.send({ temp, page });
-})
+app.get('/shoe',(req,res)=>{
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+  if(Object.keys(req.query).length != 0)
+  {
+      res.send(`Type of shoe : ${req.query.type} and color is : ${req.query.color}`)
+  }
+  else
+  {
+    res.send("All shoe will be displayed here");
+  }
+})
